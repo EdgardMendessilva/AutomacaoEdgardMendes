48bf20d53d73d62fc3ee0a844f7554eb60cde71f
EdgardMendessilva <edgard.nascimento.mendes.silva@gmail.com>
2025-03-24 23:14:32 -0300

    Atualiza README e arquivos de configuração

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,11 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: "cypress/e2e/**/*.cy.js", // Define que os arquivos .cy.js serão usados como specs
-    supportFile: "cypress/support/e2e.js", // Define o arquivo de suporte
-  },
-});
+    specPattern: "cypress/e2e/**/*.cy.js",
+    supportFile: "cypress/support/e2e.js",
+    setupNodeEvents(on, config) {
+ 
+    }
+  }
+})
