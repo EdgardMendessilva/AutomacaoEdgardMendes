b0206235a7d8ec5a88dc2bebd539f75e4396b037
EdgardMendessilva <124594028+EdgardMendessilva@users.noreply.github.com>
2025-03-25 10:17:46 -0300

    Update carrinho.cy.js

--- cypress/e2e/carrinho.cy.js
+++ cypress/e2e/carrinho.cy.js
@@ -4,12 +4,12 @@ describe('Adicionar Produto ao Carrinho', () => {
       cy.visit('https://www.saucedemo.com');
       cy.wait(2000);
 
-      // Faz login com o usuário padrão 'standard_user'
+      
       cy.get('#user-name').type('standard_user');  
       cy.get('#password').type('secret_sauce');  
       cy.get('#login-button').click();
   
-      // Valida a resposta da API de login, esperando 405 (Method Not Allowed)
+     
       cy.request({
         method: 'POST',
         url: 'https://www.saucedemo.com/api/v1/login',
@@ -17,19 +17,18 @@ describe('Adicionar Produto ao Carrinho', () => {
           username: 'standard_user',
           password: 'secret_sauce',
         },
-        failOnStatusCode: false,  // Evita que o teste falhe devido ao 405
+        failOnStatusCode: false, 
       }).then((response) => {
-        expect(response.status).to.eq(405);  // Verifica se o status da resposta é 405
+        expect(response.status).to.eq(405);  
       });
   
-      // Defina o nome do produto
       const produto1 = 'Sauce Labs Backpack';
   
-      // Selecione o produto e adicione ao carrinho
-      cy.contains(produto1).click();  // Selecione o produto na página de inventário
-      cy.get('.btn_inventory').click();  // Adiciona o produto ao carrinho
   
-      // Verifica se o produto está no carrinho
+      cy.contains(produto1).click();  
+      cy.get('.btn_inventory').click(); 
+  
+   
       cy.get('.shopping_cart_badge').should('have.text', '1');
     });
 });
